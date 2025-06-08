3a1afae29e5bf2a72c7bc6b9acc84c3347e10f2d
Nikolai Poliarnyi <PolarNick239@gmail.com>
2025-06-08 21:54:59 +0300

    configure.sh: update to Python 3.10 + OpenCV 4.11.0

--- configure.sh
+++ configure.sh
@@ -6,7 +6,7 @@
 # from APT::Periodic::Unattended-Upgrade "1";
 # to   APT::Periodic::Unattended-Upgrade "0";
 
-python_version=3.5
+python_version=3.10
 
 apt update
 apt install git cmake build-essential python3-pip python3-venv ffmpeg
@@ -16,18 +16,18 @@ cd /root/
 
 git clone https://github.com/opencv/opencv
 cd opencv
-git checkout tags/3.3.1
+git checkout tags/4.11.0
 mkdir build
 cd build
 python${python_version} -m pip install numpy
-cmake -DWITH_CUDA=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DPYTHON3_EXECUTABLE=/usr/bin/python${python_version} -DPYTHON_EXECUTABLE=/usr/bin/python${python_version} -DPYTHON_LIBRARY=/usr/lib/python${python_version}/config-${python_version}m-x86_64-linux-gnu/libpython${python_version}m.so -DPYTHON_INCLUDE_DIR=/usr/include/python${python_version}m/ ..
-make -j5
+cmake -DWITH_CUDA=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DPYTHON3_EXECUTABLE=/usr/bin/python${python_version} -DPYTHON_EXECUTABLE=/usr/bin/python${python_version} -DPYTHON_LIBRARY=/usr/lib/python${python_version}/config-${python_version}-x86_64-linux-gnu/libpython${python_version}.so -DPYTHON_INCLUDE_DIR=/usr/include/python${python_version}/ ..
+make -j16
 cd ../..
 
 python${python_version} -m venv venv
 . venv/bin/activate
 pip install -r twitchbot/requirements.txt
-cp opencv/build/lib/python3/cv2.cpython-35m-x86_64-linux-gnu.so venv/lib/python3.5/site-packages/
+cp opencv/build/lib/python3/cv2.cpython-310-x86_64-linux-gnu.so venv/lib/python${python_version}/site-packages/
 
 mkdir running
 chmod +x twitchbot/run.sh
